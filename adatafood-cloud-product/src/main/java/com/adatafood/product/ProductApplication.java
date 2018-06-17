f34a2c241fd7d91bac84a87a84d7b17cbd69daba
liangzhicheng <liangzhicheng@meizu.com>
2018-06-17 19:15:56 +0800

    123

---
+++ adatafood-cloud-product/src/main/java/com/adatafood/product/ProductApplication.java
@@ -0,0 +1,17 @@
+package com.adatafood.product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * 商品服务
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class ProductApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductApplication.class, args);
+    }
+}
